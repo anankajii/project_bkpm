e0050376a4c7381a7e55b7e99a1da41a9d73f863
anankajii <ananakun0@gmail.com>
2025-09-23 09:34:54 +0700

    update1

--- app/src/main/java/com/example/tugasbkpm/acara31/PreferenceMain.java
+++ app/src/main/java/com/example/tugasbkpm/acara31/PreferenceMain.java
@@ -6,7 +6,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tugasbkpm.R;
-
 public class PreferenceMain extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
